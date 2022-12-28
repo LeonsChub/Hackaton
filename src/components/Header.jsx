a095d1d84878f11bc68a30d95ca280164d297811
Leon <pavlenkoleon@gmail.com>
2022-12-28 15:26:32 +0200

    Adi added Sign up and Log in Modal needs to be managed by the context API

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,48 @@
 import { Popover} from '@headlessui/react'
-
+import SignUp from './SignUp'
+import { useState } from 'react';
+import Login from './Login';
+import { Link } from 'react-router-dom';
 export default function Header() {
+    const [modalOnSignUp,setModalOnSignUp] = useState(false);
+    const [modalOnLogin,setModalOnLogin] = useState(false);
+    const [loggedIn,setLoggedIn] = useState(false);
+    const handleClickedSignUp=()=>{
+        setModalOnSignUp(true)
+    }
+    const handleClickedLogin=()=>{
+        setModalOnLogin(true)
+        setModalOnSignUp(false)
+    }
+    const handleLoggedIn=()=>{
+        setLoggedIn(true)
+        setModalOnLogin(false)
+
+    }
   return (
-    <Popover className="relative bg-white">
+      <Popover className="relative bg-white">
+        {modalOnSignUp && <SignUp handleClickedLogin={()=>handleClickedLogin()} setModalOnSignUp={()=>setModalOnSignUp()}/>}
+        {modalOnLogin && <Login setModalOnLogin={()=>setModalOnLogin()} handleLoggedIn={()=>handleLoggedIn()}/>}
       <div className="mx-auto max-w-7xl px-4 sm:px-6">
         <div className="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
-            <a href="#" className='flex items-center hover:animate-bounce'>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
+            <Link to='/' className='hover:animate-bounce flex items-center'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=" w-10 h-10">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
               </svg>
 
               <h1 className='ml-2 text-lg font-medium'>myCalendar</h1>
-            </a>
+            </Link>
           </div>
-          <div className="items-center justify-end md:flex md:flex-1 lg:w-0">
-            <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
-              Sign in
-            </a>
-            <a
-              href="#"
-              className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
-            >
+         
+          <div className="items-center justify-end md:flex md:flex-1 lg:w-0"> {!loggedIn?<div>
+            <button onClick={()=>handleClickedLogin()}>
+              Log in
+            </button>
+            <button onClick={()=>handleClickedSignUp()} className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">
               Sign up
-            </a>
-          </div>
+            </button>
+         </div>:<button className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">My Events</button>} </div>
         </div>
       </div>
     </Popover>
---
+++ src/components/Login.jsx
@@ -0,0 +1,81 @@
+import {useFormik} from 'formik'
+
+const Login = (props) => { 
+    const formik=useFormik({
+    initialValues:{
+        email:'',
+        password:''
+    }
+});  
+const handleCancleClick=()=>{
+    props.setModalOnLogin(true);
+}
+
+    return ( 
+        <div>
+            <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+ 
+  <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+  <div className="fixed inset-0 z-10 overflow-y-auto">
+    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+    
+      <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+        <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+          <div className="sm:flex sm:items-start items-center justify-center">
+          
+  <div className="w-full max-w-md space-y-8 ">
+    <div >
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="  w-10 h-10">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" /> </svg>  
+             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Log in to your account</h2>
+    </div>
+    <form className="mt-8 space-y-6" action="#" method="POST">
+      <input type="hidden" name="remember" value="true"/>
+      <div className="-space-y-px rounded-md shadow-sm">
+        <div>
+          <label for="email-address" className="sr-only">Email address</label>
+          <input  onChange={formik.handleChange} id="email-address" name="email" type="email" autocomplete="email" required className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address"/>
+        </div>
+        <div>
+          <label for="password" className="sr-only">Password</label>
+          <input onChange={formik.handleChange} id="password" name="password" type="password" autocomplete="current-password" required className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Password"/>
+        </div>
+      </div>
+
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"/>
+          <label for="remember-me" className="ml-2 block text-sm text-gray-900">Remember me</label>
+        </div>
+
+        <div className="text-sm">
+          <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?</a>
+        </div>
+      </div>
+
+      <div>
+        <button onClick={()=>props.handleLoggedIn()} type="button" className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+          <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+            <svg className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+              <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
+            </svg>
+          </span>Log in
+        </button>
+      </div>
+    </form>
+  </div>
+          </div>
+        </div>
+        <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+          <button onClick={handleCancleClick} type="button" className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cancel</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+        </div>
+     );
+}
+ 
+export default Login;
